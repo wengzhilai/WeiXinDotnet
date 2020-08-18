0a797b8ccac46d53b7d8f83349982facb83ad330
wendellE450 <wendellE450@example.com>
2020-08-18 20:49:46 +0800

    修改代码公众号

--- WxProductApi/Startup.cs
+++ WxProductApi/Startup.cs
@@ -213,7 +213,7 @@ namespace WxProductApi
             }
 
             // app.UseHttpsRedirection();
-
+            app.UseStaticFiles();
             #region 使用SwaggerUI
 
             app.UseSwagger();
